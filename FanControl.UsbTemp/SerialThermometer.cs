6377684d6b7e47c7b95635d6e7e9c1c6634274a1
the-unintelligible-sphere <329282717+the-unintelligible-sphere@users.noreply.github.com>
2026-09-20 16:40:41 +0100

    Resolve an issue where it would occasionally report the temperature as 'NaN'.
    * Changed the SerialThermometer from a synchronous model that assumes the serial data arrives in one ReadLine() call, to an asynchronous one that builds up data as it is received from the sensor and parses on newlines
    * Remove build products from the project

--- FanControl.UsbTemp/SerialThermometer.cs
+++ FanControl.UsbTemp/SerialThermometer.cs
@@ -1,17 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO.Ports;
+using System.Text;
 
 namespace FanControl.UsbTemp
 {
     internal class SerialThermometer : ITempSensorDriver
     {
+        #region Private Fields
         private SerialPort _port;
-        private string _configuredPort;
+        private List<byte> _bytes = new List<byte>();
+        private Tuple<DateTime, float> _lastReading = null;
 
+        private readonly TimeSpan MAXIMUM_READING_AGE = TimeSpan.FromSeconds(2);
+
+        private readonly int MAXIMUM_READING_SIZE = 1024;
+        #endregion
+
+        #region ITempSensorDriver Implementation
         public void Open(string deviceId)
         {
-            _configuredPort = deviceId;
-
             // Try configured port first
             if (TryOpenPort(deviceId))
                 return;
@@ -27,65 +35,93 @@ namespace FanControl.UsbTemp
             // FanControl will still load the plugin
         }
 
+        public void Close()
+        {
+            try 
+            {
+                if (_port != null)
+                {
+                    _port.DataReceived -= _port_DataReceived;
+                    _port.Close();
+                }
+            } 
+            catch 
+            { 
+            }
+            _port = null;
+            _bytes = null;
+            _lastReading = null;
+        }
+
+        public float Temperature()
+        {
+            var lr = _lastReading;
+            if (lr != null && (DateTime.UtcNow - lr.Item1) > MAXIMUM_READING_AGE)
+            {
+                return float.NaN;
+            }
+
+            return lr.Item2;
+        }
+        #endregion
+
+        #region Private Methods
         private bool TryOpenPort(string portName)
         {
             try
             {
-                var p = new SerialPort(portName, 115200)
+                SerialPort port = new SerialPort(portName, 115200);
+                port.Open();
+                if (!port.IsOpen)
                 {
-                    NewLine = "\n",
-                    ReadTimeout = 300
-                };
-
-                p.Open();
-
-                // Validate by reading one line
-                string line = p.ReadLine().Trim();
-                if (float.TryParse(line, out _))
-                {
-                    _port = p;
-                    return true;
+                    Close();
                 }
 
-                p.Close();
+                _port = port;
+                _port.DataReceived += _port_DataReceived;
             }
             catch
             {
                 // ignore
             }
 
-            return false;
+            return _port != null;
         }
 
-        public void Close()
+        private void _port_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
-            try { _port?.Close(); } catch { }
+            if (e.EventType != SerialData.Chars)
+            {
+                return;
+            }
+
+            SerialPort sp = (SerialPort)sender;
+            byte[] data = new byte[sp.BytesToRead];
+            sp.Read(data, 0, data.Length);
+            foreach (byte b in data)
+            {
+                if (b == '\n')
+                {
+                    string line = Encoding.ASCII.GetString(_bytes.ToArray());
+                    _bytes.Clear();
+                    if (float.TryParse(line, out float temp))
+                    {
+                        // We correctly parsed a value, record it and the time of the last reading
+                        _lastReading = Tuple.Create(DateTime.UtcNow, temp);
+                    }
+                }
+                else if (b != '\r')
+                {
+                    _bytes.Add(b);
+                }
+
+                // Protect ourselves from exhausting RAM if we have accidentally synced up to some massive source of serial data without newlines
+                if (_bytes.Count > MAXIMUM_READING_SIZE)
+                {
+                    _bytes.Clear();
+                }
+            }
         }
-
-        public float Temperature()
-        {
-            // If port died or was never opened, try reconnecting
-            if (_port == null || !_port.IsOpen)
-            {
-                Open(_configuredPort);
-                if (_port == null)
-                    return float.NaN;
-            }
-
-            try
-            {
-                string line = _port.ReadLine().Trim();
-                if (float.TryParse(line, out float temp))
-                    return temp;
-            }
-            catch
-            {
-                // If read fails, reconnect next update
-                try { _port?.Close(); } catch { }
-                _port = null;
-            }
-
-            return float.NaN;
-        }
+        #endregion
     }
 }
